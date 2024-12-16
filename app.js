0bd70c42235ce34eebaa12f4d4260ba7a0c57c40
BalazsBanfi <banfibalazs81@gmail.com>
2024-12-16 12:13:32 +0100

    Add error class

--- app.js
+++ app.js
@@ -9,3 +9,11 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+// Every thrown error in the application or the previous middleware function calling `next` with an error as an argument will eventually go to this middleware function
+app.use((err, req, res, next) => {
+  console.error(err);
+  // We can now specify the `err.statusCode` that exists in our custom error class and if it does not exist it's probably an internal server error
+  res.status(err.statusCode || 500).send(err.message);
+});
